5ceef782ad730161a4cd981c2908b7fa9cf911a6
Marcela Fernanda <marcela.oliveira@algoritme.com.br>
2020-10-20 15:29:27 -0300

    Deployed 131d200 to 0.1.1 with MkDocs 1.1.2 and mike 0.5.3

---
+++ 0.1.1/javascripts/extra.js
@@ -0,0 +1,29 @@
+window.onclick = function(event) {
+  init(event);
+}
+
+function init (eventData) {
+  var currentId = eventData.srcElement.parentElement.hash;
+
+  if (currentId) {
+    changeCheckedInput(currentId);
+  }
+
+  break;
+}
+
+function changeCheckedInput (currentId) {
+
+  var idToUse = currentId.replaceAll('#','');
+  var cobeneficiaryInput = document.getElementById(idToUse)
+  var inputParent = cobeneficiaryInput.parentElement.getElementsByTagName("input")
+
+  cobeneficiaryInput.checked = true;
+
+  for(i=0; i<inputParent.length; i++) {
+    if (inputParent[i].id !== idToUse) {
+      inputParent[i].checked = false;
+      
+    } 
+  } 
+}
