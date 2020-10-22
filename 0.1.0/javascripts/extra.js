81f1acc5f2ce225236617be7acb9bfc151794e2d
Marcela Fernanda <marcela.oliveira@algoritme.com.br>
2020-10-22 16:00:25 -0300

    Deployed 57170d2 to 0.1.0 with MkDocs 1.1.2 and mike 0.5.3

--- 0.1.0/javascripts/extra.js
+++ 0.1.0/javascripts/extra.js
@@ -11,7 +11,6 @@ function init (eventData) {
 }
 
 function changeCheckedInput (currentId) {
-
   var idToUse = currentId.replaceAll('#','');
   var cobeneficiaryInput = document.getElementById(idToUse)
   var inputParent = cobeneficiaryInput.parentElement.getElementsByTagName("input")
@@ -20,8 +19,22 @@ function changeCheckedInput (currentId) {
 
   for(i=0; i<inputParent.length; i++) {
     if (inputParent[i].id !== idToUse) {
-      inputParent[i].checked = false;
-      
+      inputParent[i].checked = false;    
     } 
   } 
+}
+
+window.onscroll = function() {
+  displayTitle();
+}
+
+function displayTitle (){
+  var title = document.getElementsByClassName("md-header-nav__title");
+
+  if (title[0].attributes[2].value === "active") {
+    var parentElement = document.getElementsByClassName("md-header-nav__ellipsis");
+    var currentTitle = parentElement[0].children[1];
+    
+    currentTitle.classList.add("displayTitle");
+  }
 }
