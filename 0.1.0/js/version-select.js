e22c7f13b66e0306caef33936d82ed553688f37c
Marcela Fernanda <marcela.oliveira@algoritme.com.br>
2020-11-03 09:52:46 -0300

    Deployed dcb9c94 to 0.1.0 with MkDocs 1.1.2 and mike 0.5.3

--- 0.1.0/js/version-select.js
+++ 0.1.0/js/version-select.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
   // This is a bit hacky. Figure out the base URL from a known CSS file the
   // template refers to...
-  var ex = new RegExp("/?css/version-select.css$");
+  var ex = new RegExp("/?stylesheets/version-select.css$");
   var sheet = document.querySelector('link[href$="version-select.css"]');
 
   var ABS_BASE_URL = sheet.href.replace(ex, "");
@@ -34,16 +34,19 @@ window.addEventListener("DOMContentLoaded", function() {
       return {text: i.title, value: i.version};
     }), realVersion);
     select.addEventListener("change", function(event) {
-      window.location.href = ABS_BASE_URL + "/../" + this.value;
+      window.location.replace(window.location.href.replace(window.location.href.split("/")[3],this.value));
     });
 
-    var container = document.createElement("div");
+    var container = document.createElement("span");
     container.id = "version-selector";
     container.className = "md-nav__item";
     container.appendChild(select);
 
-    var sidebar = document.querySelector(".md-nav--primary > .md-nav__list");
-    sidebar.parentNode.insertBefore(container, sidebar);
+    var sidebarLast = document.querySelector(".md-header-nav__topic").lastChild;
+    var sidebar = document.querySelector(".md-header-nav__ellipsis").lastChild.parentElement.lastChild.previousSibling;
+
+    sidebarLast.parentNode.appendChild(container, sidebar);
+    sidebar.innerHTML += '<span id="version-selector" class="md-nav__item"><select class="form-control"><option value="0.1.0">0.1.0</option></select></span>';
   };
   xhr.send();
 });
